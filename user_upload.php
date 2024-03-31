dd7692c3e9edea1a6f4b559899ccb0c456f49d1c
syedtaseer <syedtaseer2011@gmail.com>
2024-03-31 14:24:52 +1100

    help command

--- user_upload.php
+++ user_upload.php
@@ -7,9 +7,26 @@
 class UserUpload
 {
 	
-	function __construct()
-	{
-		
+	function __construct() {
+
+		$sopts = "u:p:h:"; 														// define short options in directives
+		$longopts = ['file:', 'create_table', 'dry_run', 'help'];			// define long options in directives
+		$opts = getopt($sopts, $longopts);
+
+
+		if (isset($opts['help'])) {												// Help command
+			$output  = "******** Help ********\n";
+			$output .= "--file [csv file name]		This is the name of the CSV to be parsed\n";
+			$output .= "--create_table			This will cause the MySQL users table to be built (and no further action will be taken)\n";
+			$output .= "--dry_run			This will be used with the --file directive in case we want to run the script but not insert into the DB.All other functions will be executed, but the database won't be altered\n";
+			$output .= "-u				MySQL username\n";
+			$output .= "-p				MySQL password\n";
+			$output .= "-h				MySQL host\n";
+			$output .= "--help				Which will output the above list of directives with details\n";
+
+		    fwrite(STDOUT, $output);
+		}
+
 	}
 
 }
