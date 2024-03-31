44188d44e3d086f4f6be2b5f92453751aba9b428
syedtaseer <syedtaseer2011@gmail.com>
2024-03-31 19:48:34 +1100

    make foobar script

--- foobar.php
+++ foobar.php
@@ -1,3 +1,23 @@
 <?php
 
 
+$output = array();
+
+for ($i=1; $i <= 100; $i++) { 
+	if($i % 15 == 0)
+		array_push($output, 'foobar');
+	
+	elseif ($i  % 3 == 0) 
+		array_push($output, 'foo');
+
+	elseif ($i % 5 == 0) 
+		array_push($output, 'bar');
+	
+	else
+		array_push($output, $i);
+
+}
+
+fwrite(STDOUT, implode(',', $output));
+
+
